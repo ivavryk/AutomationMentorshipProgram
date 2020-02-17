13b023ceb48ea42d0e3c17bfefd1f2195a85af6d
ivavryk <igor08@meta.ua>
2020-02-17 14:44:21 +0200

    Add files via upload
    Animal sounds

---
+++ AnimalSounds/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace AnimalSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISound[] animalSounds = {new Cat(), new Dog()};
+
+            foreach (ISound animalSound in animalSounds)
+                animalSound.Sound();
+
+            Console.ReadKey();
+        }
+    }
+
+    interface ISound
+    {
+        void Sound();
+    }
+
+    class Cat : ISound
+    {
+        private string name;
+        private string colour;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Colour
+        {
+            get => colour;
+            set => colour = value;
+        }
+
+        public void Sound()
+        {
+            Console.WriteLine("M'yau");
+        }
+    }
+
+    class Dog : ISound
+    {
+        private string name;
+        private string colour;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Colour
+        {
+            get => colour;
+            set => colour = value;
+        }
+
+        public void Sound()
+        {
+            Console.WriteLine("Gau");
+        }
+    }
+}
